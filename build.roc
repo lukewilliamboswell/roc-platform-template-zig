e1135a0da241262105e1f2c36b8ac3458af665b6
Luke Boswell <lukewilliamboswell@gmail.com>
2024-11-22 09:00:24 +1100

    legacy host working

--- build.roc
+++ build.roc
@@ -6,19 +6,16 @@ import cli.Cmd
 
 main =
 
-    # build the host (cross-compile for supported targets)
     Cmd.exec "zig" ["build", "-Doptimize=ReleaseFast"]
         |> Task.mapErr! ErrBuildingHost
 
-    # copy pre-built binaries into platform
-    copyPrebuiltBinary! { from: "zig-out/lib/libmacos-aarch64.a", to: "platform/macos-arm64.a" }
-    copyPrebuiltBinary! { from: "zig-out/lib/libmacos-x86_64.a", to: "platform/macos-x64.a" }
-    copyPrebuiltBinary! { from: "zig-out/lib/liblinux-aarch64.a", to: "platform/linux-arm64.a" }
-    copyPrebuiltBinary! { from: "zig-out/lib/liblinux-x86_64.a", to: "platform/linux-x64.a" }
-    copyPrebuiltBinary! { from: "zig-out/lib/windows-aarch64.lib", to: "platform/windows-arm64.lib" }
-    copyPrebuiltBinary! { from: "zig-out/lib/windows-x86_64.lib", to: "platform/windows-x64.lib" }
+    Cmd.exec "cp" ["-f", "zig-out/lib/libhost.a", "platform/libhost.a"]
+        |> Task.mapErr! ErrCopyPrebuiltBinary
 
-copyPrebuiltBinary : { from : Str, to : Str } -> Task {} _
-copyPrebuiltBinary = \{ from, to } ->
-    Cmd.exec "cp" ["-f", from, to]
-    |> Task.mapErr \err -> ErrCopyPrebuiltBinary { from, to } err
+    ## copy pre-built binaries into platform
+    #copyPrebuiltBinary! { from: "zig-out/lib/libmacos-aarch64.a", to: "platform/macos-arm64.a" }
+    #copyPrebuiltBinary! { from: "zig-out/lib/libmacos-x86_64.a", to: "platform/macos-x64.a" }
+    #copyPrebuiltBinary! { from: "zig-out/lib/liblinux-aarch64.a", to: "platform/linux-arm64.a" }
+    #copyPrebuiltBinary! { from: "zig-out/lib/liblinux-x86_64.a", to: "platform/linux-x64.a" }
+    #copyPrebuiltBinary! { from: "zig-out/lib/windows-aarch64.lib", to: "platform/windows-arm64.lib" }
+    #copyPrebuiltBinary! { from: "zig-out/lib/windows-x86_64.lib", to: "platform/windows-x64.lib" }
--- build.zig
+++ build.zig
@@ -1,44 +1,11 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-// TODO RESTORE CROSS COMPILATION
-// const TARGETS = [_]std.zig.CrossTarget{
-//     .{ .cpu_arch = .aarch64, .os_tag = .linux },
-//     .{ .cpu_arch = .aarch64, .os_tag = .macos },
-//     .{ .cpu_arch = .aarch64, .os_tag = .windows },
-//     .{ .cpu_arch = .x86_64, .os_tag = .linux },
-//     .{ .cpu_arch = .x86_64, .os_tag = .macos },
-//     .{ .cpu_arch = .x86_64, .os_tag = .windows },
-// };
-
 pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
-    const host_target = b.standardTargetOptions(.{
-        .default_target = std.zig.CrossTarget{
-            .cpu_model = .baseline,
-            .os_tag = builtin.os.tag,
-        },
-    });
+    const host_target = b.standardTargetOptions(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "dynhost",
-        .root_source_file = .{ .path = "host/main.zig" },
-        .target = host_target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
-
-    exe.addLibraryPath(.{ .path = "." });
-    exe.linkSystemLibrary("app");
-
-    b.installArtifact(exe);
-
-    // TODO RESTORE CROSS COMPILATION
-    // for (TARGETS) |target| {
-    // const name = try std.fmt.allocPrint(b.allocator, "{s}-{s}", .{
-    //     @tagName(target.os_tag.?),
-    //     @tagName(target.cpu_arch.?),
-    // });
+    // BUILD THE LEGACY PREBUILT HOST e.g. `platform/libhost.a`, `platform/macos-aarch64.a`
 
     const lib = b.addStaticLibrary(.{
         .name = "host",
@@ -51,4 +18,34 @@ pub fn build(b: *std.Build) !void {
     lib.force_pic = true;
 
     b.installArtifact(lib);
+
+    // BUILD THE SURGICAL PREBUILT HOST e.g. `platform/host.rh`, `platform/linux-x64.rh`
+
+    // We need to build a stub roc app to dynamically link against,
+    // so we can build a surgical host
+    // const build_roc = b.addExecutable(.{
+    //     .name = "build_roc",
+    //     .root_source_file = .{ .path = "build_roc.zig" },
+    //     .target = .{}, // Empty means native.
+    //     .optimize = .Debug,
+    // });
+    // const run_build_roc = b.addRunArtifact(build_roc);
+
+    // // By setting this to true, we ensure zig always rebuilds the roc app since it can't tell if any transitive dependencies have changed.
+    // run_build_roc.stdio = .inherit;
+    // run_build_roc.has_side_effects = true;
+
+    // const exe = b.addExecutable(.{
+    //     .name = "dynhost",
+    //     .root_source_file = .{ .path = "host/main.zig" },
+    //     .target = host_target,
+    //     .optimize = optimize,
+    //     .link_libc = true,
+    // });
+
+    // exe.step.dependOn(&run_build_roc.step);
+    // exe.addLibraryPath(.{ .path = "platform/" });
+    // exe.linkSystemLibrary("app");
+
+    // b.installArtifact(exe);
 }
